f5b900c42192b9076802d8275948aaacb885092f
MuhamedSamii <hellosamysamy4@gmail.com>
2023-01-01 22:21:44 +0200

    Models Added

---
+++ Phase 2/Fawary/src/main/java/com/fawary/Fawary/Models/Credit.java
@@ -0,0 +1,15 @@
+package com.fawary.Fawary.Models;
+
+public class Credit {
+
+   public static double creditBalance ;
+
+   public double getCreditBalance() {
+      return creditBalance;
+   }
+
+   public void setCreditBalance(double creditBalance) {
+      Credit.creditBalance = creditBalance;
+   }
+
+}
