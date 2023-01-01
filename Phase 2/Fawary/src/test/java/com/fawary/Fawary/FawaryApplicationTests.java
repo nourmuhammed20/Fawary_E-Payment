47555afae3ff9cd31fbb33d20265ffb40690039a
Nour Muhammed <11410120200605@stud.cu.edu.eg>
2023-01-01 22:11:07 +0200

    Created Spring Project

---
+++ Phase 2/Fawary/src/test/java/com/fawary/Fawary/FawaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fawary.Fawary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FawaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
